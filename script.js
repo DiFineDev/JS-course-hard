ee5c3a88c421d5af4429273334fd25a1296e923d
Dmitry Komarov <dakom455@icloud.com>
2021-08-04 01:09:46 +0400

    fix error

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function hiddingString(text){
     if (typeof text !== 'string'){
         alert('Это не строка!');
     } else {
-        console.log(text.trim().substr(0,30)+'...');
+        if (text.trim().length <= 30) {
+            console.log(text.trim());
+        } else {
+            console.log(text.trim().substr(0,30)+'...');       
+        } 
     }
 }

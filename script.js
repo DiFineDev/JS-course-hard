e09186d9380fe9dc7f9a29b38387c3f1e19579de
Dmitry Komarov <dakom455@icloud.com>
2021-08-06 17:09:01 +0400

    complete homework

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-let arr = ['123450', '234900', '4329788', '234234432', '543345645', '54887298', '1234515341'];
-let arr1 = [];
-let n = 100;
+let week = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let date = new Date();
+let day = date.getDay();
 
-for (let i = 0; i < arr.length; i++) {
+let element = document.querySelector('#tag');
 
-    console.log(arr[i].substring(0,1));
-    if (arr[i].substring(0,1) === '2' || arr[i].substring(0,1) === '4') {
 
-        arr1.push(arr[i]);
+for (let i = 0; i < week.length; i++){
 
-    }
-}
-
-for (let i = 1; i <= n; i++) {
-
-    let marker = 0; 
-
-    if (i === 1) {
-        console.log(i, ' Делителем этого числа является: 1');
-        continue;
+    if (i === day) {
+        element.innerHTML += '<b>' + week[i] + '<b>' +'<br>'; 
+    } else if (i === 0 || i === 6) {
+        element.innerHTML += '<i>' + week[i] + '<i>' +'<br>'; 
+    } else {
         
+        element.innerHTML += week[i] + '<br>';
     }
-    else if (i === 2){
-        console.log(i, ` Делители этого числа: 1 и ${i}`);
-        continue;
-    } 
-    else {
-        for (let j = i-1; j > 1 && marker === 0; j--) {
-            if (i % j === 0) {
-                marker = 1;
-            }
-            
-        }
-    } 
-    if (marker === 0) {
-        console.log(i, ` Делители этого числа: 1 и ${i}`);
-    }
-
 }
-console.log(arr1);
 
 
